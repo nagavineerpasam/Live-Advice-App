8960db61540fc00c3be8e9718c4fcb471bc1332a
nagavineerpasam <nagavineerpasam@gmail.com>
2020-07-10 21:02:11 +0530

    Updates

---
+++ precache-manifest.760345ef02f1646f016c33fc9cdb572d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f45238a21d958a30cd74db69e1454a",
+    "url": "/nagavineerpasam/Live-Advice-App/index.html"
+  },
+  {
+    "revision": "022e9d8f1bc8aa3a24e4",
+    "url": "/nagavineerpasam/Live-Advice-App/static/css/main.30a6e9ac.chunk.css"
+  },
+  {
+    "revision": "97671d056a701b1692b1",
+    "url": "/nagavineerpasam/Live-Advice-App/static/js/2.b4d54544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nagavineerpasam/Live-Advice-App/static/js/2.b4d54544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022e9d8f1bc8aa3a24e4",
+    "url": "/nagavineerpasam/Live-Advice-App/static/js/main.2af952c4.chunk.js"
+  },
+  {
+    "revision": "f9d971dc44547099a6d1",
+    "url": "/nagavineerpasam/Live-Advice-App/static/js/runtime-main.a548b0b0.js"
+  },
+  {
+    "revision": "2f1b479b3dc8181d0522733fe6cae1e3",
+    "url": "/nagavineerpasam/Live-Advice-App/static/media/star.2f1b479b.jpg"
+  }
+]);
