82e865a3ee06525bf019d32731f513870f809175
nagavineerpasam <nagavineerpasam@gmail.com>
2020-07-10 20:59:23 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,51 @@
 import React from 'react';
-import Todo from './Todo';
-export default class App extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            items : [],
-            currentItem : {
-                text : '',
-                key : ''
-            }
-        }
-        this.handleInput = this.handleInput.bind(this);
-        this.addItem = this.addItem.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
-    }
-    addItem(e){
-        e.preventDefault();
-        const newItem = this.state.currentItem;
-        if(newItem.text!==""){
-        const items = [...this.state.items, newItem];
-        
-        this.setState({
-           items : items,
-           currentItem : {
-               text : '',
-               key : ''
-           }
-        })
+import './App.css';
+import axios from 'axios';
+class App extends React.Component{
+    state = {
+        advice : '',
+       
+    };
+    componentDidMount(){
+       
+        this.fetchData();
         
     }
-}
-    deleteItem(key){
-        const newItems = this.state.items.filter(item => item.key !== key);
-        this.setState({
-            items : newItems
+  
+    fetchData = () => {
+        axios.get("https://api.adviceslip.com/advice")
+        .then((response) => {
+            const {advice} = response.data.slip;
+           this.setState ({
+               advice : advice,
+              
+            })
         })
-    }
-    handleInput(e){
-        this.setState({
-            currentItem :{
-                text : e.target.value,
-                key : Date.now()
-            }
-        })
+        .catch((error)=>{
+            console.log(error);
+        });       
     }
     
     render(){
+        
+       
         return (
-            <div className="container">
-            <div className="App">
-                <h1 ><i className="fa fa-fire" style={{color:"orange",marginRight:20}}>
-                </i>TODO APP</h1><br/><br/>
-                <form onSubmit={this.addItem}>
-                <input style={{marginRight:20}} type="text" placeholder="add here..." 
-                value={this.state.currentItem.text} onChange = {this.handleInput}/>
-                <button type="submit" className="btn btn-warning">ADD</button>
-                </form><br/>
-                <Todo items={this.state.items} deleteItem={this.deleteItem}/>
-
+          
+            <div className="app">
+            
+                <div className="card">
+                 <h1 className="heading">{this.state.advice}</h1>
+                 <button className="button" onClick={this.fetchData}>
+                     <span>
+                         GIVE ME ADVICE!
+                     </span>
+                 </button>
+                 <p>*Wait for 10 sec for every advice to load*</p>
+                </div>
             </div>
-            </div>
+           
+           
         )
     }
 }
+export default App;
